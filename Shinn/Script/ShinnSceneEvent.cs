794695dba808bc99eeb02e3a49d44ed25aa87e50
SHINN\Shinn <shinn716@gmail.com>
2018-02-07 21:03:17 +0800

    Update

---
+++ Shinn/Script/ShinnSceneEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LoadScene))]
+public class ShinnSceneEvent : MonoBehaviour {
+
+	public LoadScene _load;
+	public string PressKey;
+
+	void Update () {
+		if(Input.GetKeyDown(PressKey))
+			_load.async.allowSceneActivation = true;
+	}
+
+}
