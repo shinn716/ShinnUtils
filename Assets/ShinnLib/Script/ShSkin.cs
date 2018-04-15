86666ab07ed0249c84a4c7db31f58fae43875712
Tung <shinn716@livemail.tw>
2018-04-15 17:59:04 +0800

    Add files via upload

---
+++ Assets/ShinnLib/Script/ShSkin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class ShSkin : MonoBehaviour {
+
+	Vector3[] vertices;
+
+	public Vector3[] GetMeshVertices{
+		get { return vertices;}
+	}
+
+	void Awake () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		vertices = mesh.vertices;
+	}
+
+}
