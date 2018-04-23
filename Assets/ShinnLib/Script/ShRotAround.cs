a8de9267246d2815f775ff780b3f593d957f7d6d
Tung <shinn716@livemail.tw>
2018-04-23 14:25:58 +0800

    Add files via upload

---
+++ Assets/ShinnLib/Script/ShRotAround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShRotAround : MonoBehaviour {
+
+	public float speed = 1;
+
+	[Header("Lookat")]
+	public bool lookatst = false;
+	public Transform target;
+
+	void FixedUpdate () {
+		transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+
+		if(lookatst)
+			transform.LookAt (target);
+	}
+}
