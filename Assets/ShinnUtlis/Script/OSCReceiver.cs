453ed66f2ec673cb00f3296bf51385c2cda4954b
Tung <shinn716@livemail.tw>
2018-05-03 09:28:33 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/OSCReceiver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShinnUtil{
+
+	[RequireComponent(typeof(OSC))]
+	public class OSCReceiver : MonoBehaviour {
+
+		OSC osc;
+		[Header("OSC Setting")]
+		public bool oscenable = true;
+		public string address = "/test";
+	
+		[SerializeField] 
+		int state = 0;
+		public int State {
+			get{ return state; }
+			set{ state = value; }
+		}
+	
+	
+		void Start () {
+			if (oscenable) {
+				osc = GetComponent<OSC> ();
+				osc.SetAddressHandler (address, OnReceive);
+			}
+		}
+		
+		void OnReceive(OscMessage message){
+			print ("Receive message " + message + " "+ message.values[0]);
+			string receive = message.values[0].ToString();
+			
+			if(receive=="s7"){
+				print ("trigger7");
+				state = 7;
+			}
+			if(receive=="s5"){
+				print ("trigger5");
+				state = 5;
+			}
+			if(receive=="s4"){
+				print ("trigger4");
+				state = 4;
+			}
+		}
+
+	}
+
+
+}
