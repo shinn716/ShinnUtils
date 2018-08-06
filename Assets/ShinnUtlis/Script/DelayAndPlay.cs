2d193457c59f469772e36760384b547ef7c91e0c
Tung <shinn716@livemail.tw>
2018-08-06 14:45:59 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/DelayAndPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DelayAndPlay : MonoBehaviour {
+
+    public float time = 1.5f;
+	public AudioSource _as;
+	void Start () {
+		_as.enabled = false;
+		StartCoroutine (PlayAudio(time));
+	}
+
+	IEnumerator PlayAudio(float delay){
+		yield return new WaitForSeconds (delay);
+		_as.enabled = true;
+		_as.Play ();
+	}
+
+}
