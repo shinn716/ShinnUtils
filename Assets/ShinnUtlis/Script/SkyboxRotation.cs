4a21ceb305ebedbb52edcc893b2697c0276c3748
Tung <shinn716@livemail.tw>
2018-08-07 14:18:39 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/SkyboxRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour {
+
+    public bool autoStart = false;
+    public float speed = 1;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if(autoStart)
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+}
