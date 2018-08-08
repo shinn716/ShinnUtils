39042ee709351a8af4ec3317f0dd96387f033be5
Tung <shinn716@livemail.tw>
2018-08-08 10:49:43 +0800

    Update DelayAndPlay.cs

--- Assets/ShinnUtlis/Script/DelayAndPlay.cs
+++ Assets/ShinnUtlis/Script/DelayAndPlay.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class DelayAndPlay : MonoBehaviour {
 
-    public AudioClip clip;
     public float delaytime = 1.5f;
+
+    [Range(0, 1)]
     public float volume = 1;
 
     AudioSource AS;
 
 	void Start () {
+        print("DelayAndPlay");
         AS = GetComponent<AudioSource>();
         StartCoroutine(Play(delaytime));
 	}
-
     IEnumerator Play(float time)
     {
         yield return new WaitForSeconds(time);
-        AS.PlayOneShot(clip, volume);
+        print("Play Audio");
+        int randindex = Random.Range(0, clip.Length);
+        AS.PlayOneShot(clip[randindex], volume);
     }
 
 }
