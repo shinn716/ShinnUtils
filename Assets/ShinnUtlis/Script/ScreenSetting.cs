64fac780fd7a14ed3f21ac03f4e9aaa7f9cb3668
Tung <shinn716@livemail.tw>
2018-08-08 16:08:21 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/ScreenSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSetting : MonoBehaviour {
+
+    public Vector2 ScreenResolution = new Vector2(1920, 281);
+    public bool FullScreen = true;
+    public bool FiToScreen = false;
+
+	void Start () {
+
+        if (FiToScreen)
+        {
+            Screen.SetResolution((int)ScreenResolution.x, (int)ScreenResolution.y, FullScreen);
+        }
+		
+	}
+
+}
