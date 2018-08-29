cb99acc1da3b412624f70238833f2150580e9a7a
Tung <shinn716@livemail.tw>
2018-08-29 18:44:46 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/Events/CustomEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class BoolEvent : UnityEvent<bool> { }
+[System.Serializable]
+public class IntEvent : UnityEvent<int> { }
+[System.Serializable]
+public class FloatEvent : UnityEvent<float> { }
+[System.Serializable]
+public class FloatArrayEvent : UnityEvent<float[]> { }
+[System.Serializable]
+public class ColorEvent : UnityEvent<Color> { }
+[System.Serializable]
+public class VoidEvent : UnityEvent { }
---
+++ Assets/ShinnUtlis/Script/Utility/Utility.cs
@@ -0,0 +1,13 @@
+﻿namespace Shinn
+{
+    public class Utility
+    {
+
+        public static float Map(float v, float a, float b, float x, float y)
+        {
+            return (v == a) ? x : (v - a) * (y - x) / (b - a) + x;
+        }
+
+    }
+
+}
