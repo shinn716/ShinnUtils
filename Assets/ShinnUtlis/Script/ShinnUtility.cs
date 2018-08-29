6713caa4822404697fd30a2258c69614c827ff36
Tung <shinn716@livemail.tw>
2018-08-29 18:44:03 +0800

    Delete ShinnUtility.cs

--- Assets/ShinnUtlis/Script/ShinnUtility.cs
+++
@@ -1,8 +0,0 @@
-public class ShinnUtility{
-
-    public static float Map(float v, float a, float b, float x, float y)
-    {
-        return (v == a) ? x : (v - a) * (y - x) / (b - a) + x;
-    }
-
-}
