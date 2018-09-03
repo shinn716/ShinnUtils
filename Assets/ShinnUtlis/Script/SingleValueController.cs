f80ffbfa3ffa09e0524a7c9f43854249841c8801
Tung <shinn716@livemail.tw>
2018-09-03 14:55:40 +0800

    Update SingleValueController.cs

--- Assets/ShinnUtlis/Script/SingleValueController.cs
+++ Assets/ShinnUtlis/Script/SingleValueController.cs
@@ -1,55 +1,61 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using System;
 using System.Reflection;
 
-public class SingleValueController : MonoBehaviour {
+namespace Shinn
+{
 
-    [Header("float to")]
-    public Vector2 valuerange = new Vector2(0, 1);
-    public FloatEvent floatevents;
-    //public bool isfloatevents = false;
-
-    [Header("ItweenSetting")]
-    public GameObject target;
-    public float time;
-    public float delay = 0;
-    public iTween.EaseType ease;
-    public iTween.LoopType loop;
-    public bool ignoreTimeScalest = false;
-
-    public bool complete = false;
-    public UnityEvent unityevent;
-
-    void OnEnable()
+    public class SingleValueController : MonoBehaviour
     {
 
+        [Header("float to")]
+        public Vector2 valuerange = new Vector2(0, 1);
+        public FloatEvent floatevents;
+        //public bool isfloatevents = false;
 
-        if (target == null)
-            target = gameObject;
+        [Header("ItweenSetting")]
+        public GameObject target;
+        public float time;
+        public float delay = 0;
+        public iTween.EaseType ease;
+        public iTween.LoopType loop;
+        public bool ignoreTimeScalest = false;
+
+        public bool complete = false;
+        public UnityEvent unityevent;
+
+        void OnEnable()
+        {
 
 
-       // if (isfloatevents)
-            iTween.ValueTo(target, iTween.Hash( "from", valuerange.x, "to", valuerange.y, "onupdate", "floateventsProcess",
+            if (target == null)
+                target = gameObject;
+
+
+            // if (isfloatevents)
+            iTween.ValueTo(target, iTween.Hash("from", valuerange.x, "to", valuerange.y, "onupdate", "floateventsProcess",
                                                 "time", time, "delay", delay,
                                                 "easetype", ease, "looptype", loop,
                                                 "ignoretimescale", ignoreTimeScalest,
                                                 "oncomplete", "Complete", "oncompletetarget", gameObject
                                             ));
-    }
+        }
 
 
-    void floateventsProcess(float newvalue)
-    {
-        floatevents.Invoke(newvalue);
-    }
+        void floateventsProcess(float newvalue)
+        {
+            floatevents.Invoke(newvalue);
+        }
 
-    void Complete()
-    {
-        if (complete)
-            unityevent.Invoke();
+        void Complete()
+        {
+            if (complete)
+                unityevent.Invoke();
+        }
+
     }
 
 }
