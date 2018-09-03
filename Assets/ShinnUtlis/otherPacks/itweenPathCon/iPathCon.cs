2fb013f1cc9479b999db3a8076014a12534e7c32
Tung <shinn716@livemail.tw>
2018-09-03 12:10:40 +0800

    Add files via upload
    A little tweak for itweenPathCon.

---
+++ Assets/ShinnUtlis/otherPacks/itweenPathCon/iPathCon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyButtons;
+
+namespace Shinn
+{
+    [RequireComponent(typeof(iTweenPath))]
+    public class iPathCon : MonoBehaviour
+    {
+
+        public iTweenPath ipath;
+
+        [Space]
+        public List<Transform> node = new List<Transform>();
+        Vector3[] pos;
+
+
+        [Button]
+        public void RemoveAllNode()
+        {
+            ipath.nodeCount = 0;
+        }
+
+        [Button]
+        public void RefleshItweenPath()
+        {
+            ipath.nodeCount = 0;
+            StartCoroutine(CallItweenPath());
+        }
+
+
+        IEnumerator CallItweenPath()
+        {
+            ipath.nodeCount = node.Count;
+            pos = new Vector3[node.Count];
+
+            for (int i = 0; i < node.Count; i++)
+                pos[i] = node[i].transform.position;
+
+            ipath.nodes.AddRange(pos);
+            yield return null;
+        }
+    }
+
+}
