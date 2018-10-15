29d31510e1d65d5a3dcd41b8f5ec12ba8638b387
Tung <shinn716@livemail.tw>
2018-10-15 20:45:43 +0800

    Update and rename InstantiateRaycast.cs to InstantiateRaycastObjs.cs

--- Assets/ShinnUtlis/Script/InstantiateRaycastObjs.cs
+++ Assets/ShinnUtlis/Script/InstantiateRaycastObjs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class InstantiateRaycast : MonoBehaviour {
+public class InstantiateRaycastObjs : MonoBehaviour {
 
     public enum CameraState
     {
