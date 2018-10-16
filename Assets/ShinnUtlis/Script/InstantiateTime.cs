92f4849799af3fbc1112529a537e7ab4dbcb6d0f
Tung <shinn716@livemail.tw>
2018-10-16 13:03:01 +0800

    Update and rename TimeInstantiate.cs to InstantiateTime.cs

--- Assets/ShinnUtlis/Script/InstantiateTime.cs
+++ Assets/ShinnUtlis/Script/InstantiateTime.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeInstantiate : MonoBehaviour {
+public class InstantiateTime : MonoBehaviour {
 
     public bool autoStart = true;
     public Vector2Int instTimeRange = new Vector2Int(5, 10);
