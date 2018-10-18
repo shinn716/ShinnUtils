9afb5686c7dd697237735f75c8018af03765ffc0
Tung <shinn716@livemail.tw>
2018-10-18 19:35:19 +0900

    Add files via upload

---
+++ Assets/ShinnUtlis/otherPacks/OSC/OSC_Sender.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyButtons;
+
+public class OSC_Sender : MonoBehaviour {
+
+    [System.Serializable]
+    public class SendData
+    {
+        [SerializeField] public string address = "/Character";
+        [SerializeField] public string[] data;
+    }
+
+    public SendData senddata;
+    public OSC osc;
+
+    private void Start()
+    {
+        if (osc == null)
+            osc = GetComponent<OSC>();
+    }
+
+
+    [Button]
+    public void Send() {
+        OscMessage message = new OscMessage();
+        message.address = senddata.address;
+
+        for (int i = 0; i < senddata.data.Length; i++)
+            message.values.Add(senddata.data[i]);
+
+        osc.Send(message);
+    }
+
+}
