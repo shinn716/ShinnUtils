b6dec5d5d78e5628287c6bedfbd670d6438d72fd
Tung <shinn716@livemail.tw>
2018-10-18 16:39:25 +0900

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/AnimationForTimeLineEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationForTimeLineEvent : MonoBehaviour {
+
+    public string[] actionName;
+    Animator anim;
+
+    private void Start()
+    {
+        if(anim == null)
+            anim = GetComponent<Animator>();
+    }
+
+    public void Action(int name)
+    {
+        AnimatorControllerParameter param;
+        for (int i = 0; i < anim.parameters.Length; i++)
+        {
+            param = anim.parameters[i];
+            anim.SetBool(param.name, false);
+        }
+
+        print("Action " + actionName[name]);
+        anim.SetBool(actionName[name], true);
+    }
+
+
+}
