4fc6d88d7348a6eaea17d79abb7734c64a62ff09
Tung <shinn716@livemail.tw>
2018-11-14 10:01:54 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/TimelineTools/SceneForTimelineEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneForTimelineEvents : MonoBehaviour {
+
+    public int LoadLevel = 0;
+    AsyncOperation asyncOperation;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene(LoadLevel));
+    }
+
+    public void nextScene()
+    {
+        asyncOperation.allowSceneActivation = true;
+    }
+
+
+    IEnumerator LoadScene(int level)
+    {
+        yield return null;
+        asyncOperation = SceneManager.LoadSceneAsync(level);
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Pro :" + asyncOperation.progress);
+    }
+}
