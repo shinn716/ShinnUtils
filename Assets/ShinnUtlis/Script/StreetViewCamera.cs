a955d2458e4b8c84bd19b34c9dc86fc018645f85
Tung <shinn716@livemail.tw>
2018-11-22 10:34:44 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/StreetViewCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StreetViewCamera : MonoBehaviour
+{
+    public float speed = 3.5f;
+    private float X;
+    private float Y;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+    }
+}
