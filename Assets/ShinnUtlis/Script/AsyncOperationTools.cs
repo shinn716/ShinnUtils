ecb1b7489617610dc61f29644f6f10f8e75aa32d
Tung <shinn716@livemail.tw>
2018-12-21 19:21:09 +0800

    Update AsyncOperationTools.cs

--- Assets/ShinnUtlis/Script/AsyncOperationTools.cs
+++ Assets/ShinnUtlis/Script/AsyncOperationTools.cs
@@ -1,29 +1,63 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AsyncOperationTools : MonoBehaviour {
 
-    public int currentLevel=0;
+    [ReadOnly, SerializeField]
+    int CurrentLevel = 0;
+
     public KeyCode nextKey = KeyCode.N;
+    public bool DontDestroy = false;
+    public bool ShowProcess = false;
+
+    private bool processGUI = false;
+    private bool finish = false;
+
+    AsyncOperation asyncOperation;
+    
 
     private void Start()
     {
+        if (DontDestroy)
+            DontDestroyOnLoad(gameObject);
         Next();
     }
 
     public void Next()
     {
-        if (currentLevel >= Application.levelCount - 1)
+        finish = false;
+        if (CurrentLevel > SceneManager.sceneCountInBuildSettings-2)
         {
-            currentLevel = 0;
-            StartCoroutine(LoadScene(currentLevel));
+            CurrentLevel = 0;
+            StartCoroutine(LoadScene(CurrentLevel));
         }
         else
         {
-            currentLevel++;
-            StartCoroutine(LoadScene(currentLevel));
+            CurrentLevel++;
+            StartCoroutine(LoadScene(CurrentLevel));
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (ShowProcess && processGUI)
+        {
+            float processValue = asyncOperation.progress*100;
+            if (processValue >= 90f)            
+                GUI.Label(new Rect(10, 10, 200, 50), "Finish" );            
+            else
+                GUI.Label(new Rect(10, 10, 200, 50), "Loading progress " + processValue.ToString() + "%");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(nextKey) && finish)
+        {
+            asyncOperation.allowSceneActivation = true;
+            Next();
         }
     }
 
@@ -31,33 +65,19 @@ public class AsyncOperationTools : MonoBehaviour {
     IEnumerator LoadScene(int level)
     {
         yield return null;
-
-        //Begin to load the Scene you specify
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(level);
-        //Don't let the Scene activate until you allow it to
+        processGUI = true;        
+        asyncOperation = SceneManager.LoadSceneAsync(level);
         asyncOperation.allowSceneActivation = false;
         Debug.Log("Pro :" + asyncOperation.progress);
-        //When the load is still in progress, output the Text and progress bar
+
         while (!asyncOperation.isDone)
         {
-            //Output the current progress
             print("Loading progress: " + (asyncOperation.progress * 100) + "%");
-
-            // Check if the load has finished
             if (asyncOperation.progress >= 0.9f)
             {
-                //Change the Text to show the Scene is ready
                 print("Press the " + nextKey.ToString() + " bar to continue");
-                //Wait to you press the space key to activate the Scene
-                if (Input.GetKeyDown(nextKey))
-                {
-                    //Activate the Scene
-                    asyncOperation.allowSceneActivation = true;
-                    Next();
-                }
-
+                finish = true;
             }
-
             yield return null;
         }
     }
