79f430474d38f2e7a7757a84a02af482d573b5b8
Tung <shinn716@livemail.tw>
2018-12-22 10:24:48 +0800

    Update CustomMaterialCon.cs

--- Assets/ShinnUtlis/Script/CustomMaterialCon.cs
+++ Assets/ShinnUtlis/Script/CustomMaterialCon.cs
@@ -5,36 +5,29 @@ using UnityEngine.Events;
 
 namespace Shinn
 {
-
-
     public class CustomMaterialCon : MonoBehaviour
     {
-
         public enum state
         {
             ColorControl,
             ValueControl
         }
 
-
         [Header("General setting.")]
         public Renderer[] render;
-        Material[] mat;
+        private Material[] mat;
 
         public string priority = "_emission";
         public bool autoStart = true;
         public state type;
 
         [Header("Color")]
-        //public bool conColor = false;
         public Color startColor = Color.black;
         public Color endColor = Color.white;
 
         [Header("value")]
-        //public bool conValue = false;
         public Vector2 range = new Vector2(0, 1);
-
-        [Space]
+        
         public bool ignoreTimeScalest = false;
         public float delay = 0;
         public float time = 2;
@@ -44,10 +37,8 @@ namespace Shinn
         public bool complete = false;
         public UnityEvent unityevent;
 
-
-        void Start()
+        private void Start()
         {
-
             if (render.Length == 0)
             {
                 mat = new Material[1];
@@ -64,19 +55,13 @@ namespace Shinn
             }
 
             if (autoStart)
-            {
                 Go();
-            }
-
         }
 
         public void Go()
         {
-
-
             switch (type)
             {
-
                 default:
                     break;
 
@@ -115,8 +100,7 @@ namespace Shinn
 
 
                 case state.ValueControl:
-                    for (int i = 0; i < mat.Length; i++)
-                        mat[i].SetFloat(priority, range.x);
+
                     iTween.ValueTo(gameObject, iTween.Hash("from", range.x, "to", range.y,
                                                             "onupdate", "ValueProcess",
                                                             "time", time, "delay", delay,
@@ -124,43 +108,14 @@ namespace Shinn
                                                             "ignoretimescale", ignoreTimeScalest,
                                                             "oncomplete", "Complete", "oncompletetarget", gameObject
                                               ));
+
+                    for (int i = 0; i < mat.Length; i++)
+                        mat[i].SetFloat(priority, range.x);
+
                     break;
 
             }
         }
-
-        #region itween valueto Processing
-        void ValueProcess(float newvalue)
-        {
-            for (int i = 0; i < mat.Length; i++)
-                mat[i].SetFloat(priority, newvalue);
-        }
-
-        void ColorRProcess(float newvalue)
-        {
-            for (int i = 0; i < mat.Length; i++)
-                mat[i].SetColor(priority, new Color(newvalue, mat[i].GetColor(priority).g, mat[i].GetColor(priority).b, mat[i].GetColor(priority).a));
-        }
-
-        void ColorGProcess(float newvalue)
-        {
-            for (int i = 0; i < mat.Length; i++)
-                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, newvalue, mat[i].GetColor(priority).b, mat[i].GetColor(priority).a));
-        }
-
-        void ColorBProcess(float newvalue)
-        {
-            for (int i = 0; i < mat.Length; i++)
-                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, mat[i].GetColor(priority).g, newvalue, mat[i].GetColor(priority).a));
-        }
-
-        void ColorAProcess(float newvalue)
-        {
-            for (int i = 0; i < mat.Length; i++)
-                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, mat[i].GetColor(priority).g, mat[i].GetColor(priority).b, newvalue));
-        }
-        #endregion
-
 
         void Complete()
         {
@@ -168,14 +123,49 @@ namespace Shinn
                 unityevent.Invoke();
         }
 
-
         public void Destroy()
         {
             Destroy(gameObject);
         }
 
+        private void OnDisable()
+        {
+            var itween = GetComponent<iTween>();
+            if (itween != null)
+                Destroy(itween);
+        }
 
+        #region itween valueto Processing
+        private void ValueProcess(float newvalue)
+        {
+            for (int i = 0; i < mat.Length; i++)
+                mat[i].SetFloat(priority, newvalue);
+        }
 
+        private void ColorRProcess(float newvalue)
+        {
+            for (int i = 0; i < mat.Length; i++)
+                mat[i].SetColor(priority, new Color(newvalue, mat[i].GetColor(priority).g, mat[i].GetColor(priority).b, mat[i].GetColor(priority).a));
+        }
+
+        private void ColorGProcess(float newvalue)
+        {
+            for (int i = 0; i < mat.Length; i++)
+                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, newvalue, mat[i].GetColor(priority).b, mat[i].GetColor(priority).a));
+        }
+
+        private void ColorBProcess(float newvalue)
+        {
+            for (int i = 0; i < mat.Length; i++)
+                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, mat[i].GetColor(priority).g, newvalue, mat[i].GetColor(priority).a));
+        }
+
+        private void ColorAProcess(float newvalue)
+        {
+            for (int i = 0; i < mat.Length; i++)
+                mat[i].SetColor(priority, new Color(mat[i].GetColor(priority).r, mat[i].GetColor(priority).g, mat[i].GetColor(priority).b, newvalue));
+        }
+        #endregion
     }
 
 }
