661ece9ffe98fe647244d68475e45a3073d96b7f
Tung <shinn716@livemail.tw>
2018-12-22 10:08:29 +0800

    Update CamMotionv3.cs

--- Assets/ShinnUtlis/Script/CamMotionv3.cs
+++ Assets/ShinnUtlis/Script/CamMotionv3.cs
@@ -3,57 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using EasyButtons;
 
-public class CamMotionv3 : MonoBehaviour {
-    
-	[Space]
-	public ViewPoint[] viewpoint;
+namespace Shinn
+{
 
-    public int TextIndex = 0;
-
-    [Button]
-    public void StartButton()
+    public class CamMotionv3 : MonoBehaviour
     {
-        if (viewpoint[TextIndex].isLical)
+
+        [Space]
+        public int MoveToCameraIndex = 0;
+        public ViewPoint[] viewpoint;
+
+        [Button]
+        public void StartButton()
         {
-            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[TextIndex].target.localPosition, "easetype", viewpoint[TextIndex].ease, "time", viewpoint[TextIndex].time, "delay", viewpoint[TextIndex].delaytime));
-            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[TextIndex].target.localEulerAngles, "delay", viewpoint[TextIndex].delaytime, "time", viewpoint[TextIndex].rottime));
+            if (viewpoint.Length > 0)
+                if (viewpoint[MoveToCameraIndex].isLical)
+                    MoveFunction(MoveToCameraIndex);
+                else
+                    MoveFunctionLocal(MoveToCameraIndex);
         }
-        else
+
+        public void Go(int index)
         {
-            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[TextIndex].target.position, "easetype", viewpoint[TextIndex].ease, "time", viewpoint[TextIndex].time, "delay", viewpoint[TextIndex].delaytime));
-            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[TextIndex].target.eulerAngles, "delay", viewpoint[TextIndex].delaytime, "time", viewpoint[TextIndex].rottime));
+            if (viewpoint.Length > 0)
+                if (viewpoint[index].isLical)
+                    MoveFunction(index);
+                else
+                    MoveFunctionLocal(index);
+        }
+
+        private void MoveFunction(int _index)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[_index].target.position, "easetype", viewpoint[_index].ease, "time", viewpoint[_index].time, "delay", viewpoint[_index].delaytime));
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[_index].target.eulerAngles, "delay", viewpoint[_index].delaytime, "time", viewpoint[_index].rottime));
+        }
+
+        private void MoveFunctionLocal(int _index)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[_index].target.localPosition, "easetype", viewpoint[_index].ease, "time", viewpoint[_index].time, "delay", viewpoint[_index].delaytime));
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[_index].target.localEulerAngles, "delay", viewpoint[_index].delaytime, "time", viewpoint[_index].rottime));
+        }
+
+        private void OnDisable()
+        {
+            var itween = GetComponent<iTween>();
+            if (itween != null)
+                Destroy(itween);
         }
     }
 
+    [System.Serializable]
+    public class ViewPoint
+    {
 
-	
-	public void Go(int index){
+        public Transform target;
+        public iTween.EaseType ease = iTween.EaseType.easeInOutExpo;
 
-        if (viewpoint[index].isLical)
-        {
-            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[index].target.localPosition, "easetype", viewpoint[index].ease, "time", viewpoint[index].time, "delay", viewpoint[index].delaytime));
-            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[index].target.localEulerAngles, "delay", viewpoint[index].delaytime, "time", viewpoint[index].rottime));
-        }
-        else
-        {
-            iTween.MoveTo(gameObject, iTween.Hash("position", viewpoint[index].target.position, "easetype", viewpoint[index].ease, "time", viewpoint[index].time, "delay", viewpoint[index].delaytime));
-            iTween.RotateTo(gameObject, iTween.Hash("rotation", viewpoint[index].target.eulerAngles, "delay", viewpoint[index].delaytime, "time", viewpoint[index].rottime));
-        }
-
-	}
-
-
-}
-
-[System.Serializable]
-public class ViewPoint{
-
-	public Transform target;
-	public iTween.EaseType ease = iTween.EaseType.easeInOutExpo;
-
-    public bool isLical = false;
-	public float delaytime=0;
-	public float time=5;
-	public float rottime=5;
+        public bool isLical = false;
+        [Range(0, 30)]
+        public float delaytime = 0;
+        [Range(0, 30)]
+        public float time = 5;
+        [Range(0, 30)]
+        public float rottime = 5;
+    }
 
 }
