d4b5cccc5f38de51e312bb68b5ac5704148289c5
Tung <shinn716@livemail.tw>
2018-12-22 10:09:10 +0800

    Update AsyncOperationTools.cs

--- Assets/ShinnUtlis/Script/AsyncOperationTools.cs
+++ Assets/ShinnUtlis/Script/AsyncOperationTools.cs
@@ -3,83 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AsyncOperationTools : MonoBehaviour {
+namespace Shinn
+{
 
-    [ReadOnly, SerializeField]
-    int CurrentLevel = 0;
-
-    public KeyCode nextKey = KeyCode.N;
-    public bool DontDestroy = false;
-    public bool ShowProcess = false;
-
-    private bool processGUI = false;
-    private bool finish = false;
-
-    AsyncOperation asyncOperation;
-    
-
-    private void Start()
+    public class AsyncOperationTools : MonoBehaviour
     {
-        if (DontDestroy)
-            DontDestroyOnLoad(gameObject);
-        Next();
-    }
 
-    public void Next()
-    {
-        finish = false;
-        if (CurrentLevel > SceneManager.sceneCountInBuildSettings-2)
-        {
-            CurrentLevel = 0;
-            StartCoroutine(LoadScene(CurrentLevel));
-        }
-        else
-        {
-            CurrentLevel++;
-            StartCoroutine(LoadScene(CurrentLevel));
-        }
-    }
+        static AsyncOperationTools s_Instance;
 
-    private void OnGUI()
-    {
-        if (ShowProcess && processGUI)
-        {
-            float processValue = asyncOperation.progress*100;
-            if (processValue >= 90f)            
-                GUI.Label(new Rect(10, 10, 200, 50), "Finish" );            
-            else
-                GUI.Label(new Rect(10, 10, 200, 50), "Loading progress " + processValue.ToString() + "%");
-        }
-    }
+        [ReadOnly, SerializeField]
+        private int CurrentLevel = 0;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(nextKey) && finish)
+        public KeyCode nextKey = KeyCode.N;
+        public bool DontDestroy = false;
+        public bool ShowProcess = false;
+
+        private bool processGUI = false;
+        private bool finish = false;
+
+        private AsyncOperation asyncOperation;
+
+        protected Rect viewWindow;
+
+        private void Start()
         {
-            asyncOperation.allowSceneActivation = true;
+            viewWindow = new Rect(10, 10, 150, 20);
+
+            if (DontDestroy)
+            {
+                if (s_Instance == null)
+                {
+                    s_Instance = this;
+                    DontDestroyOnLoad(gameObject);
+                }
+                else if (this != s_Instance)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
             Next();
         }
-    }
 
-
-    IEnumerator LoadScene(int level)
-    {
-        yield return null;
-        processGUI = true;        
-        asyncOperation = SceneManager.LoadSceneAsync(level);
-        asyncOperation.allowSceneActivation = false;
-        Debug.Log("Pro :" + asyncOperation.progress);
-
-        while (!asyncOperation.isDone)
+        public void Next()
         {
-            print("Loading progress: " + (asyncOperation.progress * 100) + "%");
-            if (asyncOperation.progress >= 0.9f)
+            finish = false;
+            if (CurrentLevel > SceneManager.sceneCountInBuildSettings - 2)
             {
-                print("Press the " + nextKey.ToString() + " bar to continue");
-                finish = true;
+                CurrentLevel = 0;
+                StartCoroutine(LoadScene(CurrentLevel));
             }
-            yield return null;
+            else
+            {
+                CurrentLevel++;
+                StartCoroutine(LoadScene(CurrentLevel));
+            }
         }
+
+        private void OnGUI()
+        {
+            if (ShowProcess && processGUI)
+            {
+                using (new GUILayout.HorizontalScope())
+                {
+                    viewWindow = GUILayout.Window(GetInstanceID(), viewWindow, Window, "Progress");
+                }
+            }
+        }
+
+        protected void Window(int id)
+        {
+            using (new GUILayout.VerticalScope())
+            {
+                float processValue = asyncOperation.progress * 100;
+                if (processValue >= 90f)
+                    GUILayout.Label("Finish");
+                else
+                    GUILayout.Label("Loading progress " + processValue.ToString() + "%");
+            }
+            UnityEngine.GUI.DragWindow();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(nextKey) && finish)
+            {
+                asyncOperation.allowSceneActivation = true;
+                Next();
+            }
+        }
+
+        IEnumerator LoadScene(int level)
+        {
+            yield return null;
+            processGUI = true;
+            asyncOperation = SceneManager.LoadSceneAsync(level);
+            asyncOperation.allowSceneActivation = false;
+            Debug.Log("Pro :" + asyncOperation.progress);
+
+            while (!asyncOperation.isDone)
+            {
+                print("Loading progress: " + (asyncOperation.progress * 100) + "%");
+                if (asyncOperation.progress >= 0.9f)
+                {
+                    print("Press the " + nextKey.ToString() + " bar to continue");
+                    finish = true;
+                }
+                yield return null;
+            }
+        }
+
     }
 
 }
