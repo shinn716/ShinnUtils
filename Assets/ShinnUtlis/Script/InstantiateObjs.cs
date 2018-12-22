6669a438efb66b6ee9fde970d8a675c62fe3f242
Tung <shinn716@livemail.tw>
2018-12-22 11:47:32 +0800

    Update InstantiateObjs.cs

--- Assets/ShinnUtlis/Script/InstantiateObjs.cs
+++ Assets/ShinnUtlis/Script/InstantiateObjs.cs
@@ -1,69 +1,115 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstantiateObjs : MonoBehaviour {
-
-
-    public GameObject[] prefabs;
-    public Vector2 InstianteCount = new Vector2(5, 10);
-
-    [Header("Instantiate Range")]
-    public Vector2 posxRange;
-    public Vector2 posyRange;
-    public Vector2 poszRange;
-
-    [Header("Gizmos")]
-    public float size = 10;
-    public bool WireCube = false;
-
-    void Start () {
-
-        int count = Random.Range((int)InstianteCount.x, (int)InstianteCount.y);
-        for (int i=0; i< count; i++)
-        {
-            GameObject go = (GameObject)Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
-            float posx = transform.position.x + Random.Range(posxRange.x, posxRange.y);
-            float posy = transform.position.y + Random.Range(posyRange.x, posyRange.y);
-            float posz = transform.position.z + Random.Range(poszRange.x, poszRange.y);
-            go.transform.localPosition = new Vector3(posx, posy, posz);
-            go.transform.parent = transform;
-        }
-		
-	}
-
-    private void OnDrawGizmos()
+namespace shinn
+{
+    public class InstantiateObjs : MonoBehaviour
     {
+        public GameObject[] prefabs;
+        public Vector2 InstianteCount = new Vector2(5, 10);
 
-        if (WireCube)
+        [Header("Instantiate Range")]
+        [MinMax(-100, 100, ShowEditRange = true), Space]
+        public Vector2 posxRange;
+        [MinMax(-100, 100, ShowEditRange = true), Space]
+        public Vector2 posyRange;
+        [MinMax(-100, 100, ShowEditRange = true), Space]
+        public Vector2 poszRange;
+
+        [Header("Gizmos"), Range(0, 10)]
+        public float size = 10;
+        public GizmosState mytype;
+
+        private bool MyType()
         {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
-
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
+            switch (mytype)
+            {
+                case GizmosState.Cube:
+                    return true;
+                case GizmosState.WireCube:
+                    return false;
+                default:
+                    return false;
+            }
+        }
+        public enum GizmosState
+        {
+            Cube,
+            WireCube
         }
 
-        else
+        private void Start()
         {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
-
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
-            Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
-
+            if (prefabs.Length != 0)
+            {
+                int count = Random.Range((int)InstianteCount.x, (int)InstianteCount.y);
+                for (int i = 0; i < count; i++)
+                {
+                    GameObject go = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+                    float posx = transform.position.x + Random.Range(posxRange.x, posxRange.y);
+                    float posy = transform.position.y + Random.Range(posyRange.x, posyRange.y);
+                    float posz = transform.position.z + Random.Range(poszRange.x, poszRange.y);
+                    go.transform.localPosition = new Vector3(posx, posy, posz);
+                    go.transform.parent = transform;
+                }
+            }
         }
+
+        private void OnDrawGizmos()
+        {
+            if (MyType())
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
+
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
+            }
+
+            else
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.x), Vector3.one * size);
+
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.x, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.x, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + posxRange.y, transform.position.y + posyRange.y, transform.position.z + poszRange.y), Vector3.one * size);
+            }
+        }
+
+        #region Gizmos Edit
+        [ContextMenu("ResetAllPosition")]
+        private void ResetAllPosition()
+        {
+            posxRange = Vector2.zero;
+            posyRange = Vector2.zero;
+            poszRange = Vector2.zero;
+        }
+        [ContextMenu("ResetXPosition")]
+        private void ResetXPosition()
+        {
+            posxRange = Vector2.zero;
+        }
+        [ContextMenu("ResetYPosition")]
+        private void ResetYPosition()
+        {
+            posyRange = Vector2.zero;
+        }
+        [ContextMenu("ResetZPosition")]
+        private void ResetZPosition()
+        {
+            poszRange = Vector2.zero;
+        }
+        #endregion
     }
-
-
 }
