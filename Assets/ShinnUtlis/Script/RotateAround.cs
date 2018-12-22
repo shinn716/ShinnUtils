95096f7ccd951bee6640cc80519927e5ad954e76
Tung <shinn716@livemail.tw>
2018-12-22 18:39:47 +0800

    Update RotateAround.cs

--- Assets/ShinnUtlis/Script/RotateAround.cs
+++ Assets/ShinnUtlis/Script/RotateAround.cs
@@ -1,23 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Shinn{
+namespace Shinn
+{
+    public class RotateAround : MonoBehaviour
+    {
+        public enum RotateType
+        {
+            Constant,
+            Noise
+        }
+        private float RotTypeSelect()
+        {
+            switch (rotaround)
+            {
+                case RotateType.Constant:
+                    return Time.deltaTime * Scalevalue;
 
-public class RotateAround : MonoBehaviour {
+                case RotateType.Noise:
+                    timedata += .1f;
+                    return BaseValue + NoiseScaleValue * Mathf.PerlinNoise(timedata * NoiseSpeed, 0.0F);
 
-	public float speed = 1;
+                default:
+                    return Time.deltaTime;
+            }
+        }
 
-	[Header("Lookat")]
-	public bool lookatst = false;
-	public Transform target;
 
-	void FixedUpdate () {
-		transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        public RotateType rotaround;
 
-		if(lookatst)
-			transform.LookAt (target);
-	}
-}
+        [Header("Constant"), Range(-100, 100)]
+        public float Scalevalue = 1;
+        [Header("Noise")]
+        public float BaseValue = 0;
+        public float NoiseScaleValue = 1;
+        [Range(0, 1)]
+        public float NoiseSpeed = .5f;
+        [ReadOnly]
+        public float timedata = 0;
+
+        [Header("Lookat")]
+        public bool lookat = false;
+        public Transform target;
+
+        private void FixedUpdate()
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, RotTypeSelect());
+
+            if (lookat)
+                transform.LookAt(target);
+        }
+    }
 
 }
