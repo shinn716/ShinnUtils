42b1caa433c8fc93331cd70948437d4ecb03c0c3
Tung <shinn716@livemail.tw>
2018-12-22 15:54:57 +0800

    Update NPCBehavior.cs

--- Assets/ShinnUtlis/Script/NPCBehavior.cs
+++ Assets/ShinnUtlis/Script/NPCBehavior.cs
@@ -1,66 +1,82 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ShinnUtil{
+namespace Shinn
+{
+    public class NPCBehavior : MonoBehaviour
+    {
+        [Header("NPC Moving Speed")]
+        public bool autoStart = true;
+        public bool freezeXZ = true;
+        [Range(0, 1)]
+        public float ChaseSpeed = .1f;
+        [Range(0.1f, 5)]
+        public float stopDist = 1;        
 
-public class NPCBehavior : MonoBehaviour {
+        [Header("Moving Range")]
+        public Vector2 MoveRange = new Vector2(-10, 10);
 
-		[Header("NPC Moving Speed")]
-		public float ChaseSpeed = .1f;
+        [Header("Rest Time")]
+        public Vector2 RestTimeRange = new Vector2(0, 3);
 
-		[Header("Moving Range")]
-		public float MoveRandMin = -10;
-		public float MoveRandMax = 10;
+        [Header("Show Gizmos")]
+        public bool showTarget = true;
 
-		[Header("Moving Rest Time")]
-		public float RestTimeMin = 0;
-		public float RestTimeMax = 5;
+        private Vector3 target;
+        private float timevalue;
+        private float RestTime;
 
-		[Header("Show Gizmos")]
-		public bool showTarget = true;
+        private void Start()
+        {
+            target = transform.position;
+            RestTime = Random.Range(RestTimeRange.x, RestTimeRange.y);
+        }
 
-		Vector3 target;
-		float timevalue;
-		float RestTime;
+        private void FixedUpdate()
+        {
+            if (autoStart)
+            {
+                Vector3 direction = target - transform.position;
+                if (freezeXZ)
+                    direction.y = 0;
 
-		void Start () {
-			target = transform.position;
-			RestTime = Random.Range (RestTimeMin, RestTimeMax);
-		}
-	
-		void FixedUpdate ()
-		{
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), .1f);
 
-			Vector3 direction = target - transform.position;
-			direction.y = 0;
-			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), .1f);
+                if (direction.magnitude > stopDist)
+                    transform.Translate(0, 0, ChaseSpeed);
 
-			if (direction.magnitude > 1) {
-				transform.Translate (0, 0, ChaseSpeed);
-			} else {
-				
-				timevalue += Time.fixedDeltaTime;
-				int seconds = (int)timevalue % 60;
-	
-				if(seconds>RestTime){
-					timevalue = 0;
-					RestTime = Random.Range (RestTimeMin, RestTimeMax);
-					target = new Vector3 (transform.position.x + Random.Range (MoveRandMin, MoveRandMax), 0, transform.position.z + Random.Range (MoveRandMin, MoveRandMax));
-				}
+                else
+                {
+                    timevalue += Time.fixedDeltaTime;
+                    int seconds = (int)timevalue % 60;
 
+                    if (seconds > RestTime)
+                    {
+                        timevalue = 0;
+                        RestTime = Random.Range(RestTimeRange.x, RestTimeRange.y);
 
-			}
+                        if (freezeXZ)
+                            target = new Vector3(   transform.position.x + Random.Range(MoveRange.x, MoveRange.y), 
+                                                    0, 
+                                                    transform.position.z + Random.Range(MoveRange.x, MoveRange.y));
+                        else
+                            target = new Vector3(   transform.position.x + Random.Range(MoveRange.x, MoveRange.y), 
+                                                    transform.position.y + Random.Range(MoveRange.x, MoveRange.y), 
+                                                    transform.position.z + Random.Range(MoveRange.x, MoveRange.y));
+                    }
+                }
+            }
+        }
 
-		}
+        private void OnDrawGizmos()
+        {
+            if (showTarget)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(target, .5f);
+            }
+        }
 
-
-		void OnDrawGizmos(){
-			if(showTarget){
-				Gizmos.color = Color.red;
-				Gizmos.DrawSphere (target, .5f);
-			}
-		}
-
-	}
+    }
 }
