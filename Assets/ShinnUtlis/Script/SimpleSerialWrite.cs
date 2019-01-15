c2b9d59dfd03bcc213b79f290f8dd540033e6ada
Tung <shinn716@livemail.tw>
2019-01-15 18:36:53 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/SimpleSerialWrite.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+
+public class btTest : MonoBehaviour {
+    
+    public string PORT_NAME = "COM10";
+    public int Baudrate = 9600;
+    private SerialPort sp;
+
+    void Start () {
+
+        // SerialPort.
+        try
+        {
+            sp = new SerialPort("\\\\.\\" + PORT_NAME, Baudrate);
+            sp.Open();
+            sp.ReadTimeout = 50;
+        }
+        catch (System.IO.IOException e) { }
+        catch (System.InvalidOperationException e) { }
+    }
+	
+	void Update () {
+
+        if (!sp.IsOpen) { return; }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            print("q");
+            byte[] senddata = new byte[] { (int)'q', 2};
+            sp.Write(senddata, 0, senddata.Length);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("w");
+            byte[] senddata = new byte[] { (int)'w', 2 };
+            sp.Write(senddata, 0, senddata.Length);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("e");
+            byte[] senddata = new byte[] { (int)'e', 2 };
+            sp.Write(senddata, 0, senddata.Length);
+        }
+    }
+    
+    void OnApplicationQuit()
+    {
+        if (sp != null && sp.IsOpen)
+            sp.Close();
+    }
+}
