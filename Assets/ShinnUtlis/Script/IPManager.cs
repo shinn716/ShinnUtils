df82c68431450e4af37a54d1e5c8dc1fbb834cf6
Tung <shinn716@livemail.tw>
2019-01-18 20:03:56 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/Script/IPManager.cs
@@ -0,0 +1,16 @@
+﻿public static class IPManager
+{
+    public static string GetLocalIPAddress()
+    {
+        var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
