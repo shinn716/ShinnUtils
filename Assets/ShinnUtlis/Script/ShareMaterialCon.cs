2d88a685b8d190b3ebdcbdadb229d2c0a924150c
Tung <shinn716@livemail.tw>
2019-01-24 14:32:09 +0800

    Update ShareMaterialCon.cs

--- Assets/ShinnUtlis/Script/ShareMaterialCon.cs
+++ Assets/ShinnUtlis/Script/ShareMaterialCon.cs
@@ -1,86 +1,101 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ShareMaterialCon : MonoBehaviour {
+namespace Shinn
+{
 
-    public enum type{
-        Color,
-        Float
-    }
-    private void typeSelect()
+    public class ShareMaterialCon : MonoBehaviour
     {
-        switch (mytype)
+
+        public enum type
         {
-            case type.Float:
-                iTween.ValueTo(gameObject, iTween.Hash("from", FromAndTo.x, "to", FromAndTo.y, "onupdate", "valueto", "time", time, "delay", delay, "easetype", ease, "looptype", loop, "oncomplete", "complete"));
-                break;
-            case type.Color:
-                iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.r, "to", ToColor.r, "onupdate", "valuetoR", "time", time, "delay", delay, "easetype", ease, "looptype", loop, "oncomplete", "complete"));
-                iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.g, "to", ToColor.g, "onupdate", "valuetoG", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
-                iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.b, "to", ToColor.b, "onupdate", "valuetoB", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
-                iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.a, "to", ToColor.a, "onupdate", "valuetoA", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
-                break;
-                
+            Color,
+            Float
         }
-    }
-    public type mytype;
+        private void typeSelect()
+        {
+            switch (mytype)
+            {
+                case type.Float:
+                    iTween.ValueTo(gameObject, iTween.Hash("from", FromAndTo.x, "to", FromAndTo.y, "onupdate", "valueto", "time", time, "delay", delay, "easetype", ease, "looptype", loop, "oncomplete", "complete"));
+                    break;
+                case type.Color:
+                    iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.r, "to", ToColor.r, "onupdate", "valuetoR", "time", time, "delay", delay, "easetype", ease, "looptype", loop, "oncomplete", "complete"));
+                    iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.g, "to", ToColor.g, "onupdate", "valuetoG", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
+                    iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.b, "to", ToColor.b, "onupdate", "valuetoB", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
+                    iTween.ValueTo(gameObject, iTween.Hash("from", FromColor.a, "to", ToColor.a, "onupdate", "valuetoA", "time", time, "delay", delay, "easetype", ease, "looptype", loop));
+                    break;
 
-    [Header("Material")]
-    public Material mat;
-    public string property;
+            }
+        }
+        public type mytype;
 
-    [Header("Itween")]
-    public Vector2 FromAndTo = new Vector2(0, 1);
-    public Color FromColor = Color.black;
-    public Color ToColor = Color.white;
+        [Header("Material")]
+        public Material mat;
+        public string property;
 
-    [Space, Range(0, 5)]
-    public float delay = 0;
-    public float time = 1;
-    public iTween.EaseType ease;
-    public iTween.LoopType loop;
+        [Header("Itween valueto (from and to)")]
+        public Vector2 FromAndTo = new Vector2(0, 1);
 
-    [Header("Complete")]
-    public UnityEvent events;
+        [Space]
+        public Color FromColor = Color.black;
+        public Color ToColor = Color.white;
 
-    private void OnEnable () {
-        typeSelect();
-    }
+        [Space, Range(0, 5)]
+        public float delay = 0;
+        public float time = 1;
+        public iTween.EaseType ease;
+        public iTween.LoopType loop;
 
-    private void valueto(float newvalue)
-    {
-        mat.SetFloat(property, newvalue);
-    }
+        [Header("Complete")]
+        public UnityEvent events;
 
-    private void valuetoR(float newvalue)
-    {
-        Color _matColor = mat.GetColor(property);
-        mat.SetColor(property, new Color(newvalue, _matColor.g, _matColor.b, _matColor.a));
-    }
+        private void OnEnable()
+        {
+            if (mat == null)
+            {
+                Debug.LogWarning("Please assign material.");
+                return;
+            }
+            typeSelect();
+        }
 
-    private void valuetoG(float newvalue)
-    {
-        Color _matColor = mat.GetColor(property);
-        mat.SetColor(property, new Color(_matColor.r, newvalue, _matColor.b, _matColor.a));
-    }
+        private void valueto(float newvalue)
+        {
+            mat.SetFloat(property, newvalue);
+        }
 
-    private void valuetoB(float newvalue)
-    {
-        Color _matColor = mat.GetColor(property);
-        mat.SetColor(property, new Color(_matColor.r, _matColor.g, newvalue, _matColor.a));
-    }
+        private void valuetoR(float newvalue)
+        {
+            Color _matColor = mat.GetColor(property);
+            mat.SetColor(property, new Color(newvalue, _matColor.g, _matColor.b, _matColor.a));
+        }
 
-    private void valuetoA(float newvalue)
-    {
-        Color _matColor = mat.GetColor(property);
-        mat.SetColor(property, new Color(_matColor.r, _matColor.g, _matColor.b, newvalue));
-    }
+        private void valuetoG(float newvalue)
+        {
+            Color _matColor = mat.GetColor(property);
+            mat.SetColor(property, new Color(_matColor.r, newvalue, _matColor.b, _matColor.a));
+        }
 
-    private void complete()
-    {
-        events.Invoke();
+        private void valuetoB(float newvalue)
+        {
+            Color _matColor = mat.GetColor(property);
+            mat.SetColor(property, new Color(_matColor.r, _matColor.g, newvalue, _matColor.a));
+        }
+
+        private void valuetoA(float newvalue)
+        {
+            Color _matColor = mat.GetColor(property);
+            mat.SetColor(property, new Color(_matColor.r, _matColor.g, _matColor.b, newvalue));
+        }
+
+        private void complete()
+        {
+            events.Invoke();
+        }
+
     }
 
 }
