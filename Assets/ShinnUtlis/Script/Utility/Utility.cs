aca83d1fd9efb1a6cc488154932155f4fc64cef5
Tung <shinn716@livemail.tw>
2019-02-14 16:11:11 +0800

    Update Utility.cs

--- Assets/ShinnUtlis/Script/Utility/Utility.cs
+++ Assets/ShinnUtlis/Script/Utility/Utility.cs
@@ -55,6 +55,23 @@ namespace Shinn
                     return ip.ToString(); 
             throw new System.Exception("No network adapters with an IPv4 address in the system!");
         }
+	    
+	/// <summary>
+        /// CopyComponent function, from https://answers.unity.com/questions/458207/copy-a-component-at-runtime.html
+        /// </summary>
+	public Component CopyComponent(Component original, GameObject destination)
+    	{
+        	System.Type type = original.GetType();
+        	Component copy = destination.AddComponent(type);
+        	// Copied fields can be restricted with BindingFlags
+        	System.Reflection.FieldInfo[] fields = type.GetFields();
+        	foreach (System.Reflection.FieldInfo field in fields)
+        	{
+            	field.SetValue(copy, field.GetValue(original));
+        	}
+        	return copy;
+    	}
+	    
     }
 
 }
