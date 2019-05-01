90d50b8bf1c0f3cfda30d86674eb2bbe6015577c
Tung <shinn716@livemail.tw>
2019-05-01 22:34:38 +0800

    Add files via upload

---
+++ Assets/ShinnUtlis/scripts/DisableEvt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DisableEvt : MonoBehaviour {
+
+    public UnityEvent events;
+
+    public Vector3 orgpos;
+    public Color orgcolor = Color.white;
+
+    private void OnDisable()
+    {
+        events.Invoke();
+    }
+
+    
+    public void PosSetDefault()
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        if (rect == null)
+            transform.localPosition = orgpos;
+        else
+            rect.localPosition = orgpos;
+    }
+
+    public void ColorSetDefault()
+    {
+        SpriteRenderer sp = GetComponent<SpriteRenderer>();
+        sp.color = orgcolor;
+    }
+}
