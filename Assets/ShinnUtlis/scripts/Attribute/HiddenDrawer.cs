7c82ee6d72ec41e6703b86fee404bde3bf34a986
Tung <shinn716@livemail.tw>
2019-06-25 17:31:08 +0800

    Add files via upload
    Gradient

---
+++ Assets/ShinnUtlis/scripts/Attribute/HiddenDrawer.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+
+[UsedImplicitly]
+public class HiddenDrawer : MaterialPropertyDrawer
+{
+    public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+    {
+    }
+
+    public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
+    {
+        return -EditorGUIUtility.standardVerticalSpacing;
+    }
+}
---
+++ Assets/ShinnUtlis/scripts/Attribute/UIGradientShaderGUI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+ 
+// ReSharper disable once CheckNamespace
+// ReSharper disable once InconsistentNaming
+[UsedImplicitly]
+public class UIGradientShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+     
+        EditorGUILayout.Space();
+ 
+        var material = materialEditor.target as Material;
+        if (material == null)
+            throw new ArgumentNullException(nameof(material));
+ 
+        var colorKeys = Enumerable
+            .Range(0, material.GetInt("_Colors"))
+            .Select((s, t) => new GradientColorKey(material.GetColor($"_Color{t}"), material.GetFloat($"_ColorTime{t}")))
+            .ToArray();
+ 
+        var alphaKeys = Enumerable
+            .Range(0, material.GetInt("_Alphas"))
+            .Select((s, t) => new GradientAlphaKey(material.GetFloat($"_Alpha{t}"), material.GetFloat($"_AlphaTime{t}")))
+            .ToArray();
+ 
+        var gradient = new Gradient();
+        gradient.SetKeys(colorKeys, alphaKeys);
+ 
+        EditorGUI.BeginChangeCheck();
+ 
+        var field = EditorGUILayout.GradientField("Gradient", gradient);
+ 
+        if (!EditorGUI.EndChangeCheck())
+            return;
+ 
+        for (var i = 0; i < 8; i++)
+        {
+            material.SetColor($"_Color{i}", Color.magenta);
+            material.SetFloat($"_ColorTime{i}", -1.0f);
+            material.SetFloat($"_Alpha{i}", -1.0f);
+            material.SetFloat($"_AlphaTime{i}", -1.0f);
+        }
+ 
+        for (var i = 0; i < field.colorKeys.Length; i++)
+        {
+            material.SetColor($"_Color{i}", field.colorKeys[i].color);
+            material.SetFloat($"_ColorTime{i}", field.colorKeys[i].time);
+        }
+ 
+        for (var i = 0; i < field.alphaKeys.Length; i++)
+        {
+            material.SetFloat($"_Alpha{i}", field.alphaKeys[i].alpha);
+            material.SetFloat($"_AlphaTime{i}", field.alphaKeys[i].time);
+        }
+ 
+        material.SetInt("_Colors", field.colorKeys.Length);
+ 
+        material.SetInt("_Alphas", field.alphaKeys.Length);
+ 
+        EditorUtility.SetDirty(material);
+    }
+}
