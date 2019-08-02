f952cbf504c160bff5a3f0b2e002724798dac6a8
Tung <shinn716@livemail.tw>
2019-08-02 11:39:47 +0800

    Add files via upload
    Upload Math.

---
+++ Assets/ShinnUtlis/scripts/Utility/MathUtils.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Shinn
+{
+    public class MathUtils
+    {
+        /// <summary>
+        /// 取偶數, 小數點第N位 (小數點最大第二位)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static float Even(float input, int n = 1)
+        {
+            input = (float)((int)(input * Mathf.Pow(10, n)) / Mathf.Pow(10, n));
+            input *= Mathf.Pow(10, n);
+            return input % 2 == 0 ? input / Mathf.Pow(10, n) : (input + 1) / Mathf.Pow(10, n);
+        }
+
+        /// <summary>
+        /// 取偶數
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int Even(int input)
+        {
+            return input % 2 == 0 ? input : (input + 1);
+        }
+    }
+}
