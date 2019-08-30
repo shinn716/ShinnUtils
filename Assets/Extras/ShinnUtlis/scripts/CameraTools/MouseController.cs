e6f058e9d27b453a87c334f3ed5e7434238c3cc1
Tung <shinn716@livemail.tw>
2019-08-30 11:53:20 +0800

    Add files via upload
    Upload MouseController.cs.

---
+++ Assets/Extras/ShinnUtlis/scripts/CameraTools/MouseController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Shinn.CameraTools
+{
+    public class MouseController : MonoBehaviour
+    {
+        private enum MouseSate
+        {
+            WheelRoll,
+            CenterDrag,
+            RightDrag
+        }
+
+        private MouseSate mouseSate;
+
+        private float speed = .1f;
+
+        public float ScrollWheel { get { return Input.GetAxis("Mouse ScrollWheel"); } }
+
+        [ContextMenu("YawSetZero")]
+        public void YawSetZero()
+        {
+            transform.localEulerAngles = Vector3.Scale(transform.localEulerAngles, new Vector3(1, 1, 0));
+        }
+
+        private void StateSelect(MouseSate state)
+        {
+            float h = 0, v = 0;
+            switch (state)
+            {
+                case MouseSate.RightDrag:
+                    h = speed * 30 * -Input.GetAxis("Mouse Y");
+                    v = speed * 30 * Input.GetAxis("Mouse X");
+                    transform.Rotate(h, v, 0);
+                    break;
+                case MouseSate.CenterDrag:
+                    h = speed * -Input.GetAxis("Mouse X");
+                    v = speed * -Input.GetAxis("Mouse Y");
+                    transform.Translate(h, v, 0);
+                    break;
+                case MouseSate.WheelRoll:
+                    transform.Translate(0, 0, ScrollWheel);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+                StateSelect(MouseSate.RightDrag);
+            if (Input.GetMouseButton(2))
+                StateSelect(MouseSate.CenterDrag);
+            else
+                StateSelect(MouseSate.WheelRoll);
+        }
+    }
+}
