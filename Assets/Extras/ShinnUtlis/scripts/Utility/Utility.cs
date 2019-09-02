2e6fed12585d53470be4b89f9ebc0b43725ed558
Tung <shinn716@livemail.tw>
2019-09-02 16:27:07 +0800

    Update Utility.cs
    Upload StringToQuaternion function.

--- Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
+++ Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
@@ -166,7 +166,25 @@ namespace Shinn
             }
         }
         #endregion
+            
+        public static Quaternion StringToQuaternion(string sQuaternion)
+        {
+            // Remove the parentheses
+            if (sQuaternion.StartsWith("(") && sQuaternion.EndsWith(")"))
+            {
+                sQuaternion = sQuaternion.Substring(1, sQuaternion.Length - 2);
+            }
+            // split the items
+            string[] sArray = sQuaternion.Split(',');
 
+            // store as a Vector3
+            Quaternion result = new Quaternion(
+                float.Parse(sArray[0]),
+                float.Parse(sArray[1]),
+                float.Parse(sArray[2]),
+                float.Parse(sArray[3]));
+            return result;
+            }
+        }
     }
-
 }
