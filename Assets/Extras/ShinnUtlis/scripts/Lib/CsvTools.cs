f7f9e3b3715408d3d53d4d1151bb18b455adf316
Tung <shinn716@livemail.tw>
2019-09-18 16:34:14 +0800

    Update CsvTools.cs

--- Assets/Extras/ShinnUtlis/scripts/Lib/CsvTools.cs
+++ Assets/Extras/ShinnUtlis/scripts/Lib/CsvTools.cs
@@ -1,5 +1,5 @@
-﻿// Author : Shinn
-// Date : 20190913
+// Author : Shinn
+// Date : 20190918
 // Reference : https://sushanta1991.blogspot.com/2015/02/how-to-write-data-to-csv-file-in-unity.html
 // 
 
@@ -13,20 +13,67 @@ namespace Shinn.Common
 
     public class CsvTools
     {
+        private List<string[]> rowData = new List<string[]>();
+
         public bool ClearCsvAtWrite { get; set; }
 
-        private string[] csvContent;
-        private List<string[]> rowData = new List<string[]>();
-        private string saveName;
-        
-        public CsvTools(string[] content, string name)
+        public string ReadCsv(string path)
         {
-            csvContent = content;
-            saveName = name;
+            string text = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    text = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            catch (System.NullReferenceException e)
+            {
+                Debug.Log("資料格式錯誤");
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("The file could not be read:");
+            }
+            return text;
         }
 
+        public double[] ReadCsvDouble(string path)
+        {
+           List<double> result = new List<double>();
+            string text = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    text = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            catch (System.NullReferenceException e)
+            {
+                Debug.Log("資料格式錯誤");
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("The file could not be read:");
+            }
 
-        public void WriteToCsv()
+            string[] dataStr = text.Trim().Split('\n');
+            for (int i=0; i< dataStr.Length; i++)
+            {
+                string[] rawQ = dataStr[0].Split(',');
+                result.Add(double.Parse(rawQ[0]));
+                result.Add(double.Parse(rawQ[1]));
+                result.Add(double.Parse(rawQ[2]));
+                result.Add(double.Parse(rawQ[3]));
+            }
+            return result.ToArray();
+        }
+
+        public void WriteToCsv(string[] csvContent, string saveName)
         {
             if (ClearCsvAtWrite)
                 rowData.Clear();
@@ -41,15 +88,15 @@ namespace Shinn.Common
             }
 
             int length = output.GetLength(0);
-            string delimiter = ",";
+            string delimiter = "";
 
             StringBuilder sb = new StringBuilder();
-
             for (int index = 0; index < length; index++)
+            {
                 sb.AppendLine(string.Join(delimiter, output[index]));
+            }
 
-
-            string filePath = GetPath();
+            string filePath = GetPath(saveName);
 
             StreamWriter outStream = File.CreateText(filePath);
             outStream.WriteLine(sb);
@@ -62,16 +109,16 @@ namespace Shinn.Common
         }
 
         // Following method is used to retrive the relative path as device platform
-        private string GetPath()
+        private string GetPath(string name)
         {
 #if UNITY_EDITOR
-            return Application.streamingAssetsPath + "/csv/" + saveName + ".csv";
-// #elif UNITY_ANDROID
-//         return Application.persistentDataPath+"Saved_data.csv";
-// #elif UNITY_IPHONE
-//         return Application.persistentDataPath+"/"+"Saved_data.csv";
-// #else
-//         return Application.dataPath +"/"+"Saved_data.csv";
+            return Application.streamingAssetsPath + "/csv/" + name + ".csv";
+#elif UNITY_ANDROID
+        return Application.persistentDataPath+"Saved_data.csv";
+#elif UNITY_IPHONE
+        return Application.persistentDataPath+"/"+"Saved_data.csv";
+#else
+        return Application.dataPath +"/"+"Saved_data.csv";
 #endif
         }
     }
