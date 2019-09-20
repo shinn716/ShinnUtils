3d9cae0e1eaaad0c1741c2b7b7cb7de44f3f8f91
Tung <shinn716@livemail.tw>
2019-09-20 18:38:35 +0800

    Update UDPServer.cs
    Fixed issue of receive speed.

--- Assets/Extras/ShinnUtlis/scripts/Lib/UdpSocket/UDPServer.cs
+++ Assets/Extras/ShinnUtlis/scripts/Lib/UdpSocket/UDPServer.cs
@@ -1,82 +1,94 @@
-﻿// Author : Shinn
+// Author : Shinn
 // Date : 20190905
-// Reference : http://kimdicks.blogspot.com/2017/11/unityudp.html
-//
+// Reference :  http://kimdicks.blogspot.com/2017/11/unityudp.html
+//              https://blog.csdn.net/NippyLi/article/details/79123609
+//              https://stackoverflow.com/questions/8321271/c-sharp-slow-socket-speed            
 
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
-namespace Shinn.Commom
+namespace Shinn.Common
 {
     public class UDPServer
     {
+        public string Ip { get; set; } = IPAddress.Any.ToString();
+        public int Port { get; set; } = 10000;
         public string Output { get; set; }
 
-        private IPEndPoint ipEndPoint;
-        private UdpClient udpClient;
-        private Thread receiveThread;
+        private const int SendBufferLength = 524266;
 
-        private byte[] receiveByte;
-        private string receiveData = string.Empty;
-        private bool threadStart;
+        private Socket socket;
+        private EndPoint clientEnd;
+        private IPEndPoint ipEnd;
+        private Thread connectThread;
 
-        public UDPServer(string ip = "0.0.0.0", int port = 10000)
+        private string recvStr;
+        private string sendStr;
+        private byte[] recvData = new byte[SendBufferLength];
+        private byte[] sendData = new byte[SendBufferLength];
+        private int recvLen;
+        
+        public UDPServer()
         {
-            //Debug.Log("UDPServer");
-            ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
-            udpClient = new UdpClient(ipEndPoint.Port);
+            Debug.Log("UDPServer");
+            ipEnd = new IPEndPoint(ToInt(Ip), Port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(ipEnd);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            clientEnd = (EndPoint)sender;
+            Debug.Log("waiting for UDP dgram");
         }
 
-        /// <summary>
-        /// Init thread.
-        /// </summary>
         public void Init()
         {
-            //Debug.Log("Init");
-            receiveThread = new Thread(ReceiveData);
-            receiveThread.IsBackground = true;
-            receiveThread.Start();
-            threadStart = true;
+            Debug.Log("Init");
+            connectThread = new Thread(new ThreadStart(SocketReceive));
+            connectThread.Start();
         }
 
-        /// <summary>
-        /// Stop thread.
-        /// </summary>
         public void Dispose()
         {
-            //Debug.Log("Dispose");
-            threadStart = false;
-
-            if (udpClient != null)
+            Debug.Log("Dispose");
+            if (connectThread != null)
             {
-                ((System.IDisposable)udpClient).Dispose();
-                udpClient.Close();
-                udpClient = null;
+                connectThread.Interrupt();
+                connectThread.Abort();
             }
-
-            if (receiveThread != null)
-            {
-                receiveThread.Join();
-                receiveThread = null;
-            }
-
-            System.GC.SuppressFinalize(this);
+            if (socket != null)
+                socket.Close();
         }
 
-        /// <summary>
-        /// Receive data.
-        /// </summary>
-        private void ReceiveData()
+        private void SocketSend(string sendStr)
         {
-            while (threadStart)
+            sendData = new byte[SendBufferLength];
+            sendData = Encoding.ASCII.GetBytes(sendStr);
+            socket.SendTo(sendData, sendData.Length, SocketFlags.None, clientEnd);
+        }
+
+        private void SocketReceive()
+        {
+            while (true)
             {
-                receiveByte = udpClient.Receive(ref ipEndPoint);
-                receiveData = System.Text.Encoding.UTF8.GetString(receiveByte);
-                Output = receiveData;
-                Debug.Log("Receive: " + receiveData);
+                recvData = new byte[SendBufferLength];
+                recvLen = socket.ReceiveFrom(recvData, ref clientEnd);
+                //Debug.Log("message from: " + clientEnd.ToString());
+                recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
+                //Debug.Log(recvStr);
+                sendStr = "From Server: " + recvStr;
+                //SocketSend(sendStr);
+                Output = sendStr;
             }
+        }
+
+        private long ToInt(string addr)
+        {
+            // careful of sign extension: convert to uint first;
+            // unsigned NetworkToHostOrder ought to be provided.
+            return (long)(uint)IPAddress.NetworkToHostOrder(
+                 (int)IPAddress.Parse(addr).Address);
         }
     }
 }
