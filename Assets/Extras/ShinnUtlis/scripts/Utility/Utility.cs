a262a1decb24d0051e3888b5508963009b3f96d7
Tung <shinn716@livemail.tw>
2019-10-01 23:56:28 +0800

    Update Utility.cs
    add FindInActiveObjectByName function.

--- Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
+++ Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
@@ -196,5 +196,23 @@ namespace Shinn
             value = value > 180 ? value - 360 : value;
             return value;
         }
+        
+        public GameObject FindInActiveObjectByName(string name)
+        {
+            Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+            for (int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i].hideFlags == HideFlags.None)
+                {
+                    if (objs[i].name == name)
+                    {
+                        return objs[i].gameObject;
+                    }
+                }
+            }
+            return null;
+        }
+        
+        
     }
 }
