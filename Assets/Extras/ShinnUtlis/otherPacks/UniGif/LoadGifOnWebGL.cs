646a6c3d94a011d8e9e10b02bab1811223076c46
Shinn <>
2019-11-13 23:38:19 +0800

    For test

---
+++ Assets/Extras/ShinnUtlis/otherPacks/UniGif/LoadGifOnWebGL.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class LoadGifOnWebGL : MonoBehaviour
+{
+    public UniGifImage uniGifImage;
+
+    private void Start()
+    {
+        string assetPath = Application.streamingAssetsPath;
+        bool isWebGl = assetPath.Contains("://") ||
+                         assetPath.Contains(":///");
+        try
+        {
+            if (isWebGl)
+            {
+                string path = Path.Combine(assetPath, "001.gif");
+                print(path);
+                uniGifImage.SetGifFromUrl(path);
+                uniGifImage.Play();
+            }
+            else // desktop app
+            {
+                uniGifImage.SetGifFromUrl("001.gif");
+                uniGifImage.Play();
+            }
+        }
+        catch
+        {
+            // handle failure
+        }
+
+    }
+}
---
+++ Assets/Sample/angle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://answers.unity.com/questions/525952/how-i-can-converting-a-quaternion-to-a-direction-v.html?_ga=2.181103651.562719474.1569940126-381981505.1566652556
+
+public class angle : MonoBehaviour
+{
+    public Transform cube1;
+    public Transform cube2;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // 四元數 算夾角
+        Vector3 targetForward = cube1.transform.rotation * Vector3.up;
+        Vector3 targetUp = cube2.transform.rotation * Vector3.up;
+
+        cube2.RotateAround(cube1.transform.position, Vector3.up, 20 * Time.deltaTime);
+
+        print(Vector3.Angle(targetUp, targetForward));
+    }
+}
