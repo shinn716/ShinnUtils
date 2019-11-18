f14aba7796ab29e7d14d6e786c26e10834e88c44
Tung <shinn716@livemail.tw>
2019-11-18 23:59:22 +0800

    Update Utility.cs
    Upload  UniCodeTools

--- Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
+++ Assets/Extras/ShinnUtlis/scripts/Utility/Utility.cs
@@ -29,6 +29,26 @@ namespace Shinn
         }
     }
     #endregion
+    
+    public class UniCodeTools
+    {
+        public static string Unicode2String(string source)
+        {
+            return new Regex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled).Replace(
+                         source, x => string.Empty + Convert.ToChar(Convert.ToUInt16(x.Result("$1"), 16)));
+        }
+
+        public static string String2Unicode(string source)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(source);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                stringBuilder.AppendFormat("\\u{0}{1}", bytes[i + 1].ToString("x").PadLeft(2, '0'), bytes[i].ToString("x").PadLeft(2, '0'));
+            }
+            return stringBuilder.ToString();
+        }
+    }
 
     public class Utility
     {
