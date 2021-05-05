267da29f4603b2c8868fba5995514be1ae5594c1
John_tsai <john.tc.tsai@deltaww.com>
2021-05-05 16:24:40 +0800

    Upload Serial receiver and assetbundleSample.

---
+++ Assets/Extras/ShinnUtlis/scripts/AssetBoundleSample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class AssetBoundleSample : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Load_gameObject(System.IO.Path.Combine(Application.streamingAssetsPath, "myObject")));
+    }
+    
+    #region Private
+    private IEnumerator Load_gameObject(string url)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(url, 0))
+        {
+            yield return uwr.SendWebRequest();
+            if (uwr.error != null)
+            {
+                throw new Exception("WWW download error: " + uwr.error);
+            }
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+            GameObject go = bundle.LoadAsset(bundle.name) as GameObject;
+            Instantiate(go);
+        }
+    }
+    #endregion
+}
