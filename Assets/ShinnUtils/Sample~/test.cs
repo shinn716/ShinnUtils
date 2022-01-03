f1625c9b4d0230235ca21e663578c4c4b8dbbd85
DELTA\john.tc.tsai <john.tc.tsai@deltaww.com>
2022-01-03 10:56:18 +0800

    Update for 0.0.4

---
+++ Assets/ShinnUtils/Sample~/test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Shinn.Common;
+using System.Threading;
+
+public class test : MonoBehaviour
+{
+    private LoadXml loadXml_Email;
+    private CsvTools csvTools;
+
+    void Start()
+    {
+        loadXml_Email = new LoadXml(new List<string> { "SMTP_Client", "SMTP_Port", "USER", "USER_Pass", "To", "Subject", "Body", "AttachFile" });
+        loadXml_Email.Load(ShUnityPath.ApplicationStreamingAssetsPath, "EmailSetting.xml");
+        
+        LoadFile.LoadAllFiles(ShUnityPath.ApplicationStreamingAssetsPath, ShExtension.XML);
+
+        csvTools = new CsvTools();
+    }
+    
+    private void OnApplicationQuit()
+    {
+        loadXml_Email.Clear();
+    }
+
+    [ContextMenu("Test_WriteToCsv")]
+    private void Test_WriteToCsv()
+    {
+
+        string[] title = { "A", "B", "C", "D" };
+        csvTools.WriteToCsv(title, "test");
+    }
+}
