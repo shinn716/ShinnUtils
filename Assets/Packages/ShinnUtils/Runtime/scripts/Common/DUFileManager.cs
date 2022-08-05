d6ef9028a95b1c1e57a466b1119284eaf6fc0d4d
john.tc.tsai\DEV <shinn716@livemail.tw>
2022-08-05 17:19:04 +0800

    Fix.

---
+++ Assets/Packages/ShinnUtils/Runtime/scripts/Common/DUFileManager.cs
@@ -0,0 +1,93 @@
+﻿//
+//  Upload.cs
+//  Upload
+//
+//  Created by Shinn on 2019/9/01.
+//  Copyright © 2019 Shinn. All rights reserved.
+//
+//
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.IO;
+
+namespace Shinn
+{
+    public class DUFileManager : MonoBehaviour
+    {
+        public static DUFileManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public IEnumerator DownloadCo(string _sourceUrl, string _downloadLocation, Action<float> _process = null, Action _complete = null)
+        {
+            var uri = new Uri(_sourceUrl);
+            var path = Path.Combine(_downloadLocation, Path.GetFileName(uri.LocalPath));
+
+            if (File.Exists(path))
+            {
+                Debug.Log("[File exist] " + path);
+                yield return null;
+            }
+            else
+            {
+                var uwr = new UnityWebRequest(_sourceUrl)
+                {
+                    method = UnityWebRequest.kHttpVerbGET
+                };
+
+                var dh = new DownloadHandlerFile(path)
+                {
+                    removeFileOnAbort = true
+                };
+
+                uwr.downloadHandler = dh;
+                uwr.SendWebRequest();
+
+                while (!uwr.isDone)
+                {
+                    _process?.Invoke(uwr.downloadProgress);
+                    yield return null;
+                }
+
+                if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+                    Debug.LogError("[Error]，" + uwr.error);
+                else
+                {
+                    _process?.Invoke(100);
+                    _complete?.Invoke();
+                }
+            }
+        }
+
+        public IEnumerator UploadCo(string _destinationUrl, string _fullUrl, Action<float> _process = null, Action _complete = null)
+        {
+            WWWForm postForm = new WWWForm();
+            byte[] content = File.ReadAllBytes(_fullUrl);
+            string filename = Path.GetFileName(_fullUrl);
+            string[] filenameWithoutExtension = filename.Split(char.Parse("."));
+
+            postForm.AddBinaryData("theFile", content, filenameWithoutExtension[0], "text/plain");
+            UnityWebRequest uwr = UnityWebRequest.Post(_destinationUrl, postForm);
+            yield return uwr.SendWebRequest();
+
+            while (!uwr.isDone)
+            {
+                _process?.Invoke(uwr.uploadProgress);
+                yield return null;
+            }
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+                Debug.LogError(uwr.error);
+            else
+            {
+                _process?.Invoke(100);
+                _complete?.Invoke();
+            }
+        }
+    }
+}
